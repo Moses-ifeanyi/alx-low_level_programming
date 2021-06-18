0b94552a9529bde84c976086d777a167cc8ad5a8
Moses ifeanyi <moses.ifeanyim@gmail.comgit config --global user.name MosesMosesgit config --global user.email moses.ifeanyim@gmail.com>
2021-06-18 11:20:47 -0700

    updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
-
 /**
  * main - Prints String
  * Description: "\"Programming is like building a multilingual puzzle"
  * Return: 0
  */
-
 int main(void)
 {
   puts("\"Programming is like building a multilingual puzzle");
