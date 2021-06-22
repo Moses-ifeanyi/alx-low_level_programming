0b0fbe508e96f7e8501e653ce37d547db2f13ffc
Moses ifeanyi <moses.ifeanyim@gmail.comgit config --global user.name MosesMosesgit config --global user.email moses.ifeanyim@gmail.com>
2021-06-22 14:40:29 -0700

    updated

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,8 @@
 #include "holberton.h"
 
 /**
- *print_alphabet_x10 - prints the alphabet 10 times
- **/
-
+*print_alphabet_x10 - prints the alphabet 10 times
+**/
 void print_alphabet_x10(void)
 {
 int x;
