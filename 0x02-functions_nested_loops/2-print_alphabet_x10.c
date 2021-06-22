60771ba6a7324faf6a652479483d689d98ec241c
Moses ifeanyi <moses.ifeanyim@gmail.comgit config --global user.name MosesMosesgit config --global user.email moses.ifeanyim@gmail.com>
2021-06-22 14:58:30 -0700

    updated

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,18 +3,18 @@
 /**
 *print_alphabet_x10 - prints the alphabet 10 times
 **/
+
 void print_alphabet_x10(void)
 {
 int x;
 char alp;
-
 for (x = 1; x <= 10; x++)
 {
 for (alp = 'a'; alp <= 'z'; alp++)
-
+{
 _putchar(alp);
 }
 _putchar('\n');
 }
-return;
+return (0);
 }
