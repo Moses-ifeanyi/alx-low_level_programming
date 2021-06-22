6dfb15b064b0f5a3c63d35a01fae0d7989d19e11
Moses ifeanyi <moses.ifeanyim@gmail.comgit config --global user.name MosesMosesgit config --global user.email moses.ifeanyim@gmail.com>
2021-06-22 13:51:41 -0700

    a function that prints the alphabet, in lowercase, followed by a new line

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+*print_alphabet - prints alphabet
+**/
+void print_alphabet(void)
+{
+char alpha;
+for (alpha = 'a'; alpha <= 'z'; alpha++)
+{
+_putchar(alpha);
+}
+_putchar('\n');
+
+return;
+}
