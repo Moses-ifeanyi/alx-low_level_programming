c79a52db232858b8e3fb12733e92ea8798bf4400
Moses ifeanyi <moses.ifeanyim@gmail.comgit config --global user.name MosesMosesgit config --global user.email moses.ifeanyim@gmail.com>
2021-06-22 12:47:12 -0700

    a program that prints Holberton, followed by a new line.

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,37 +1,28 @@
 #include "holberton.h"
-
 /**
-
-* main - prints Holberton
-
-* Return: 0
-
-*/
+*main - entry point
+*
+*Return: Always 0
+**/
 
 int main(void)
   
 {
-  
 _putchar('H');
-  
-_putchar('o');
-  
-_putchar('l');
-  
-_putchar('b');
-  
-_putchar('e');
-  
-_putchar('r');
-  
-_putchar('t');
-  
-_putchar('o');
-  
-_putchar('n');
-  
-_putchar('\n');
 
+_putchar('o');
+
+_putchar('l');
+
+_putchar('b');
+
+_putchar('e');
+
+_putchar('r');
+
+_putchar('t');
+_putchar('o');
+_putchar('n');
+_putchar('\n');
 return (0);
-  
 }
